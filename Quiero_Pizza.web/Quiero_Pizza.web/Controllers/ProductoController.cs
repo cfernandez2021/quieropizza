2c4537764c328ee0e8d71f40e942c36786f10b60
carlos fernandez <cfernandezd@unah.hn>
2022-01-29 17:36:13 -0600

    Agregando productos

---
+++ Quiero_Pizza.web/Quiero_Pizza.web/Controllers/ProductoController.cs
@@ -0,0 +1,41 @@
+﻿using Quiero_Pizza.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiero_Pizza.web.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Pizza 4 Estaciones";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Pizza Jamón y Queso";
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "Pizza De Peperoni";
+
+            var producto4 = new ProductoModel();
+            producto4.Id = 4;
+            producto4.Descripcion = "Alistas Picantes";
+
+            var listadeproductos = new List<ProductoModel>();
+            listadeproductos.Add(producto1);
+            listadeproductos.Add(producto2);
+            listadeproductos.Add(producto3);
+            listadeproductos.Add(producto4);
+            
+            return View(listadeproductos);
+        }
+    }
+}
