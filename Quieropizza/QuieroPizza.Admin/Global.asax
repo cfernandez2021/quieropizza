52a5cb33a66696801fddc6416150c5ea859a2e29
carlos fernandez <cfernandezd@unah.hn>
2022-01-30 12:30:37 -0600

    Agregando Cambios y Tablas

---
+++ Quieropizza/QuieroPizza.Admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuieroPizza.Admin.MvcApplication" Language="C#" %>
--- Quieropizza/Quieropizza.web/Controllers/ProductosController.cs
+++ Quieropizza/Quieropizza.web/Controllers/ProductosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuieroPizza.BL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,34 +12,10 @@ namespace Quieropizza.web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
+            var productosbl = new ProductosBL();
+            var listadeproductos = productosbl.obtenerProductos();
 
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Pizza Jamón y Queso";
-            producto1.Precio = 280;
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Pizza Hut Chesee";
-            producto2.Precio = 190;
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Pizza 6 Quesos";
-            producto3.Precio = 260;
-
-            var producto4 = new ProductoModel();
-            producto4.Id = 4;
-            producto4.Descripcion = "Alistas de Pollo";
-            producto4.Precio = 120;
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-            listadeProductos.Add(producto4);
-
-            return View(listadeProductos);
+            return View(listadeproductos);
         }
     }
 }
